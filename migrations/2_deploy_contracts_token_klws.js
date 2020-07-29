46ac1e49af304935b6f9a5927b521c84ffd84ddb
vpomogalov <>
2020-07-29 17:23:21 +0300

    main owner

--- migrations/2_deploy_contracts_token_klws.js
+++ migrations/2_deploy_contracts_token_klws.js
@@ -2,6 +2,6 @@ const KlwsToken = artifacts.require('./KlwsToken.sol');
 
 module.exports = (deployer) => {
     //http://www.onlineconversion.com/unix_time.htm
-    var owner = "0x8087300524FB758a3092046D9E975cd0f6D8a521";
+    var owner = "0x78d3a9fC256bb9a81d4fB887187D403c8347792e";
     deployer.deploy(KlwsToken, owner);
 };
